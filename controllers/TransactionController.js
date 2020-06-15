fc999b97054bd9a4d528ff5fc5aff8081cdf4c97
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-15 16:18:37 -0300

    logs added to controller create method, and new method to get transactions added to controller

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,9 +1,19 @@
 module.exports = {
     create: function (req,res) {
-        TransactionService.create();
+        if(!req.body.message)
+            return res.status(400).json({
+                msg: `Can not save a transaction with an empty message.`
+            })
+
+        console.log(`\n\nReceived new transaction with message ${ req.body.message}`);
+        TransactionService.create(req.body.message);
 
         return res.json({
             status: "Your transaction is being calculated"
         });
+    },
+
+    get: (req, res) => {
+        return res.json(TransactionService.get());
     }
 }

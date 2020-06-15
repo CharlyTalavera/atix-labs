7ae0a2e589163cae8ddad5cdab888db0a649a95d
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-15 16:21:48 -0300

    child code to process by brute force the hashes values

---
+++ child/child.js
@@ -0,0 +1,19 @@
+const TransactionService = require('../services/TransactionService.js');
+
+process.on("message", function(json){
+    let i = 0;
+    let hash = "";
+    const data = JSON.parse(json);
+    console.log(`\n\nCalculating new hash for message ${data.message}`);
+    while( !hash.startsWith('00000') ){
+        hash = TransactionService.hash(data.hash, data.message, i);
+        i++;
+    }
+    process.send(JSON.stringify({
+        hash,
+        message: data.message,
+        nonce: i - 1
+    }));
+
+    process.exit();
+});

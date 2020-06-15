4eceb2b539168ec2b1cd3642a5cd8485ce340f36
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-15 16:21:25 -0300

    default value when creating first entry added to transaction service; console logs added

--- services/TransactionService.js
+++ services/TransactionService.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
+FIRST_HASH_VALUE = '0000000000000000000000000000000000000000000000000000000000000000';
+FIRST_NONCE = '0';
 
-function create() {
+function create(message) {
     return new Promise( async (resolve, reject) => {
         var child = require('child_process').fork('child/child.js');
         child.on("message", async function(result){
@@ -9,22 +11,31 @@ function create() {
             
             if(!pushed){
                 console.log(`Transaction can't be pushed, generating new hash...`);
-                return create();
+                return create(message);
             }
-
+            console.log(`Ok, transaction saved.`);
             resolve(data);
         });
 
         const lastTransaction = await TransactionService.last();
 
-        child.send(JSON.stringify({hash:lastTransaction.hash, message: 'something'}));
+        if(_.isEmpty(lastTransaction.hash)){
+            await StorageService.save(`${FIRST_HASH_VALUE},${message},${FIRST_NONCE}`);
+            console.log('Saved first transaction.');
+            return resolve({
+                hash: FIRST_HASH_VALUE,
+                message,
+                nonce: FIRST_NONCE
+            });
+        }
+
+        child.send(JSON.stringify({hash:lastTransaction.hash, message}));
     })
 }
 
 module.exports = {
     last: async () => {
         const line = (await StorageService.getLastLine()).split(',');
-
         return {
             hash: line[0],
             message: line[1],
@@ -34,14 +45,15 @@ module.exports = {
 
     push: async function (hash, message, nonce) {
         const prev = (await StorageService.getLastLine()).split(',');
-        console.log("Checking current calculated hash with last file hash entry");
+
         const lastHash = this.hash(prev[0], message, nonce);
+        console.log(`\n\nSaving transaction, cheking if last hash matchs with current... message: ${message}`);
         console.log(hash, lastHash);
         if(hash !== lastHash){
-            console.log(`Conflict found, last hash doesn't match with current. Other process won us.`);
+            console.log(`FAILED. Conflict found, last hash doesn't match with current. Other process won us.`);
             return false;
         }
-        console.log(`Hashes matches, saving transaction...`);
+        console.log(`OK. Hashes matches, saving transaction...`);
         await StorageService.save(`${hash},${message},${nonce}`);
         return true;
     },
@@ -50,5 +62,9 @@ module.exports = {
 
     hash: (hash, message, nonce) => {
         return crypto.createHash('sha256').update(`${hash},${message},${nonce}`).digest('hex').toString();
+    },
+
+    get: () => {
+        return StorageService.getAll();
     }
 }

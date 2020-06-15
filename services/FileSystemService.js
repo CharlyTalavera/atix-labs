cc597b3221a6e7623df73fa45f08f6c9a4850b63
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-15 16:20:37 -0300

    get all method added to file system service, and if file does not exist, return an empty value

--- services/FileSystemService.js
+++ services/FileSystemService.js
@@ -3,10 +3,26 @@ const fs = require('fs');
 
 module.exports = {
     getLastLine: () => {
-        return readLastLines.read(config.storage_path, 1);
+        return readLastLines.read(config.storage_path, 1)
+            .catch((e) => {
+                if(e.toString() === 'Error: file does not exist') // If file does not exist, return empty string.
+                    return "";
+                throw e;
+            });
     },
 
     save(line){
-        fs.appendFileSync(config.storage_path, `\n${line}`);
+        fs.appendFileSync(config.storage_path, `${line}\n`); // If file does not exist, it will be created.
+    },
+
+    getAll(){
+        try{
+            const result = fs.readFileSync(config.storage_path).toString().split('\n').filter(line => !_.isEmpty(line));
+            return result;
+        }
+        catch(e){
+            console.log(e);
+            return [];
+        }
     }
 }

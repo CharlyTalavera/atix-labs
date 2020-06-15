5cdc498f263a20183a8f0d961ea82524cf3a446f
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-14 23:37:16 -0300

    storage file renamed to transactions.txt

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ const STORAGE_DIR = process.env.STORAGE || 'storage';
 module.exports = {
     port: process.env.PORT,
     routes: `${__dirname}/routes.js`,
-    storage_path: `./${STORAGE_DIR}/block.txt`,
+    storage_path: `./${STORAGE_DIR}/transactions.txt`,
     storage_service: process.env.STORAGE_SERVICE || 'FileSystem',
     controllers: `./controllers`,
     services: `./services`

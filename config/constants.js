6b543881b631cd6c820ee0c4dd7b1c415c85549f
Charly Talavera <talaveracarlosd@gmail.com>
2020-06-15 18:24:10 -0300

    default port added to config

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 const STORAGE_DIR = process.env.STORAGE || 'storage';
 
 module.exports = {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     routes: `${__dirname}/routes.js`,
     storage_path: `./${STORAGE_DIR}/transactions.txt`,
     storage_service: process.env.STORAGE_SERVICE || 'FileSystem',
